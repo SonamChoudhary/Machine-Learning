c65e78b8b5bc3d500d56c9e3db2c05a6b16b22a4
Sonam Choudhary <31292156+SonamChoudhary@users.noreply.github.com>
2018-02-18 19:23:26 -0700

    Add files via upload

---
+++ HW2/3_3/perceptron_epoch_3(3).sh
@@ -0,0 +1,5 @@
+python perceptron_epoch_3(3).py 0 a5a.train a5a.train >output_train.txt
+python perceptron_epoch_3(3).py 0 a5a.train a5a.test >output_test.txt
+python perceptron_epoch_3(3).py 1 a5a.train a5a.train >output_margin_train.txt
+python perceptron_epoch_3(3).py 1 a5a.train a5a.test > output_margin_test.txt
+
